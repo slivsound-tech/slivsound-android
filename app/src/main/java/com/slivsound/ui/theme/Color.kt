2eae613f6b64e2b6fcab9c6781bd0d02abbe51cd
Viktor0024 <vk8198340@gmail.com>
2025-09-15 12:06:06 +0200

    edited colors

--- app/src/main/java/com/slivsound/ui/theme/Color.kt
+++ app/src/main/java/com/slivsound/ui/theme/Color.kt
@@ -2,19 +2,18 @@ package com.slivsound.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val BackgroundStart =Color (0xFF3A3F5F)
 val Background = Color(0xFF2A2E45)
 val Surface = Color(0xFF3B3F58)
 val SurfaceVariant = Color(0xFF4A4F6A)
 
-val SurfaceDimmed = Color(0x404a4f6a)
+val SurfaceDimmed = Color(0x474A4F6A)
 
 val Primary = Color(0xFFA889E6)
 val OnPrimary = Color(0xFFFFFFFF)
 
-val TextPrimary = Color(0xFFF6F1EB)
-val TextSecondary = Color(0xFFB7BBC9)
+val ContentPrimary = Color(0xFFF6F1EB)
+val ContentSecondary = Color(0xFFB7BBC9)
 val Outline = Color(0xFF44475C)
 val Error = Color(0xFFE07A5F)
 val Success = Color(0xFF9ED67B)
-val White = Color(0xFFFFFFFF)
-val Black = Color(0xFF000000)
