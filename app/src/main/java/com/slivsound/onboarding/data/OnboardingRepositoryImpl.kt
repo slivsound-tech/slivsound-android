5a64b6b1816f90649f45c33d1aa01ef96c179556
CRESAXIC <cresaxic@gmail.com>
2025-10-02 23:58:54 +0300

    UI welcome

---
+++ app/src/main/java/com/slivsound/onboarding/data/OnboardingRepositoryImpl.kt
@@ -0,0 +1,5 @@
+package com.slivsound.onboarding.data
+
+import com.slivsound.onboarding.domain.OnboardingRepository
+
+class OnboardingRepositoryImpl : OnboardingRepository
---
+++ app/src/main/java/com/slivsound/onboarding/presentation/OnboardingWelcomeViewModel.kt
@@ -0,0 +1,30 @@
+package com.slivsound.onboarding.presentation
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import org.koin.android.annotation.KoinViewModel
+
+data class OnboardingWelcomeState(
+    val isLoading: Boolean = false
+)
+
+sealed interface OnboardingWelcomeEvent
+
+sealed interface OnboardingWelcomeEffect
+
+@KoinViewModel
+class OnboardingWelcomeViewModel : ViewModel() {
+
+    private val _state = MutableStateFlow(OnboardingWelcomeState())
+    val state: StateFlow<OnboardingWelcomeState> = _state.asStateFlow()
+
+    private val _effects = MutableSharedFlow<OnboardingWelcomeEffect>(extraBufferCapacity = 1)
+    val effects: Flow<OnboardingWelcomeEffect> = _effects.asSharedFlow()
+
+    fun onEvent(event: OnboardingWelcomeEvent) {}
+}
